4fbb9859f78a712a4a3cd512e8bcdc4f7e4d82fa
Somakin Mykola <k.semakin@gmail.com>
2019-07-15 18:49:21 +0300

    HV_v02

--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import theme from './js/theme';
 import menuItem from './menu.json';
 import menuItemTemplate from './templates/menu.hbs';
 
-//const oneItem = menuItemTemplate(menuItem[1]);
-
 const refs = {
   menuList: document.getElementById('menu'),
 };
-// refs.menuList.insertAdjacentHTML('beforeend', oneItem);
 buildMenuFeed(menuItem);
 function buildMenuFeed(menuItem) {
   const markup = menuItem.map(menu => menuItemTemplate(menu)).join(' ');
